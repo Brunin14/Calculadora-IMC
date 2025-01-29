9dc8d2486768a179f71c450ca4a1188e435722d4
Bruno <brunoalvesclaro16@gmail.com>
2025-01-29 19:27:10 -0300

    Primeiro commit

---
+++ inedx.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector("#btn");
+
+
+function peso(){
+    const pesoInput = document.getElementById("pesos").value;
+    const alturaInput = document.getElementById("altura").value;
+
+    if (!pesoInput || !alturaInput || (pesoInput) || (alturaInput)) {
+        document.getElementById("resultado").innerHTML = "Por favor, insira valores válidos!";
+        return;
+    }
+
+    let contaF = pesoInput / (alturaInput * alturaInput);
+    console.log("IMC calculado:", contaF.toFixed(2));
+    
+
+    if (contaF <= 18.5){
+        mensagem = "Abaixo do peso: "
+    } else if (contaF <= 24.9){
+        mensagem = "Peso normal: ";
+    } else if (contaF <= 29.9){
+        mensagem = "Sobrepeso (acima da média): ";
+    } else {
+        mensagem = "Obesidade: ";
+    }
+
+    document.getElementById("resultado").innerHTML = mensagem + contaF.toFixed(2);
+}
+
+btn.addEventListener("click", peso)
